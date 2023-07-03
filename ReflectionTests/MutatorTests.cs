c472e4b190b91a899e67fea3e2d1eaf859742ac2
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-07-03 10:22:25 +0200

    Updated tests to cover both a regular setter and a init-setter

--- ReflectionTests/MutatorTests.cs
+++ ReflectionTests/MutatorTests.cs
@@ -5,69 +5,118 @@ namespace ReflectionTests;
 public class MutatorTests
 {
 	class C {
-		public string? Value { get; init; }
+		public string? RegularSetter { get; set; }
+		public string? InitSetter { get; init; }
 	}
 
 	record R
 	{
-		public string? Value { get; init; }
+		public string? RegularSetter { get; set; }
+		public string? InitSetter { get; init; }
 	}
 
 	struct S
 	{
-		public string? Value { get; init; }
+		public string? RegularSetter { get; set; }
+		public string? InitSetter { get; init; }
 	}
 
 	[Fact]
-	public void Mutate__classPassedIn__valueIsMutated()
+	public void Mutate__classPassedIn_regularSetterMutated__valueIsChanged()
 	{
 		var mutator = new Mutator();
 
 		var c = new C {
-			Value = "original value",
+			RegularSetter = "original value",
 		};
 
-		// The following does not compile, because the prop is { get; init; }
-		//c.Value = "";
+		mutator.Mutate(c, x => x.RegularSetter, "new value");
 
-		// but this mutates it anyway, because { init; } is a compile-time check and does nothing at runtim
-		mutator.Mutate(c, x => x.Value, "new value");
-
-		Assert.Equal("new value", c.Value);
+		Assert.Equal("new value", c.RegularSetter);
 	}
 
 	[Fact]
-	public void Mutate__structPassedIn__valueIsNotMutated()
+	public void Mutate__classPassedIn_initSetterMutated__valueIsNotChanged()
+	{
+		var mutator = new Mutator();
+
+		var c = new C
+		{
+			InitSetter = "original value",
+		};
+
+		// The following does not compile, because the prop is { get; init; }
+		//c.InitSetter = "";
+
+		// but this mutates it anyway, because { init; } is a compile-time check and does nothing at runtim
+		mutator.Mutate(c, x => x.InitSetter, "new value");
+
+		Assert.Equal("original value", c.InitSetter);
+	}
+
+	[Fact]
+	public void Mutate__structPassedIn_regularSetter__valueIsNotMutated()
 	{
 		var mutator = new Mutator();
 
 		var s = new S
 		{
-			Value = "original value",
+			RegularSetter = "original value",
 		};
 
 		// this still mutates the passed-in struct, but since it is a copy, the original is unchanged
-		mutator.Mutate(s, x => x.Value, "new value");
+		mutator.Mutate(s, x => x.RegularSetter, "new value");
 
-		Assert.Equal("original value", s.Value);
+		Assert.Equal("original value", s.RegularSetter);
 	}
 
 	[Fact]
-	public void Mutate__recordPassedIn__valueIsMutated()
+	public void Mutate__structPassedIn_initSetter__valueIsNotMutated()
+	{
+		var mutator = new Mutator();
+
+		var s = new S
+		{
+			InitSetter = "original value",
+		};
+
+		// this still mutates the passed-in struct, but since it is a copy, the original is unchanged
+		mutator.Mutate(s, x => x.InitSetter, "new value");
+
+		Assert.Equal("original value", s.InitSetter);
+	}
+
+	[Fact]
+	public void Mutate__recordPassedIn_regularSetter__valueIsChanged()
 	{
 		var mutator = new Mutator();
 
 		var r = new R
 		{
-			Value = "original value",
+			RegularSetter = "original value",
+		};
+
+		mutator.Mutate(r, x => x.RegularSetter, "new value");
+
+		Assert.Equal("new value", r.RegularSetter);
+	}
+
+	[Fact]
+	public void Mutate__recordPassedIn_initSetter__valueIsNotMutated()
+	{
+		var mutator = new Mutator();
+
+		var r = new R
+		{
+			InitSetter = "original value",
 		};
 
 		// The following does not compile, because the prop is { get; init; }
-		//r.Value = "";
+		//r.InitSetter = "";
 
 		// but this mutates it anyway, because { init; } is a compile-time check and does nothing at runtim
-		mutator.Mutate(r, x => x.Value, "new value");
+		mutator.Mutate(r, x => x.InitSetter, "new value");
 
-		Assert.Equal("new value", r.Value);
+		Assert.Equal("original value", r.InitSetter);
 	}
 }
